688b50e460171f2f52a6531707205f697c833255
appWon <korkrip11@gmail.com>
2024-08-23 06:55:42 +0900

    init

---
+++ src/main/java/com/won/StoreManageMent/StoreManageMentApplication.java
@@ -0,0 +1,13 @@
+package com.won.StoreManageMent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManageMentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManageMentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/won/StoreManageMent/StoreManageMentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.won.StoreManageMent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManageMentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
