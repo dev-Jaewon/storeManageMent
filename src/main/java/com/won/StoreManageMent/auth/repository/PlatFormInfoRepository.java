5f49c1affb58a3da78d962af2de48e448cf46853
dev-jaewon <app235naver@gmail.com>
2024-08-27 03:26:02 +0900

    rename 플랫폼 저장 db 위한 명칭 변경

--- src/main/java/com/won/StoreManageMent/auth/repository/PlatFormInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.won.StoreManageMent.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.won.StoreManageMent.auth.entity.PlatformInfoEntity;
-
-public interface PlatFormInfoRepository extends JpaRepository<PlatformInfoEntity, Long> {
-    
-}
