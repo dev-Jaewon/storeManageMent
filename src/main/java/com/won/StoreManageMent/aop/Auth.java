ad5bf88cfbde1adf1b34bed1d32f1f79f315d764
dev-jaewon <app235naver@gmail.com>
2024-08-28 07:48:31 +0900

    feat 토큰인증방법 configFilter -> aop로 변경

---
+++ src/main/java/com/won/StoreManageMent/aop/Auth.java
@@ -0,0 +1,8 @@
+package com.won.StoreManageMent.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+public @interface Auth {
+}
---
+++ src/main/java/com/won/StoreManageMent/aop/AuthAop.java
@@ -0,0 +1,36 @@
+package com.won.StoreManageMent.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+
+import com.won.StoreManageMent.common.jwt.JwtProvider;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+
+@Aspect
+@Component
+@AllArgsConstructor
+public class AuthAop {
+    
+    public final JwtProvider jwtProvider;
+    private final HttpServletRequest request;
+
+    @Before("@annotation(Auth)")
+    public void checkAuth(){
+        String Authorization = request.getHeader("Authorization");
+
+        if (Authorization == null || !Authorization.startsWith("Bearer ")) {
+            throw new AccessDeniedException("접근권한이 없습니다.");
+        }
+
+        String token = Authorization.substring(7);
+        String resultCheck = jwtProvider.checkToken(token);
+
+        if (resultCheck == null) {
+            throw new AccessDeniedException("접근권한이 없습니다.");
+        }
+    }
+}
